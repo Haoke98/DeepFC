bf0c322fb5c90dd3ec5d6429964c1a1bfec1f27f
Sadam·Sadik <1903249375@qq.com>
2024-11-13 12:38:05 +0800

    优化了文件大小输出,控制精确度在保留2位小数
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- tools.py
+++ tools.py
@@ -1,8 +1,9 @@
-def sum(l:list[dict],key=""):
+def sum(l: list[dict], key=""):
     res = 0
     for i in l:
-        res+=i[key]
+        res += i[key]
     return res
+
 
 def storageFormat(v):
     """
@@ -10,21 +11,21 @@ def storageFormat(v):
     :param v: 存储大小的值（注：转换成Byte字节时的数值）,永远只接受正数和零。
     :return: 可打印的字符串形式
     """
-    if v<0:
+    if v < 0:
         raise Exception("该方法不接受负数")
-    a = v/1024
+    a = v / 1024
     if a < 1:
-        return f"{v}B"
-    elif a>1 and a<1024:
-        return f"{a}KB"
+        return f"{v:.2f}B"
+    elif 1 < a < 1024:
+        return f"{a:.2f}KB"
     else:
-        b = a/1024
-        if b>1 and b<1024:
-            return f"{b}MB"
-        elif b >1024:
-            c = b/1024
-            if c>1 and c< 1024:
-                return f"{c}GB"
-            elif c> 1024:
-                d = c/1024
-                return f"{d}TB"
+        b = a / 1024
+        if 1 < b < 1024:
+            return f"{b:.2f}MB"
+        elif b > 1024:
+            c = b / 1024
+            if 1 < c < 1024:
+                return f"{c:.2f}GB"
+            elif c > 1024:
+                d = c / 1024
+                return f"{d:.2f}TB"
